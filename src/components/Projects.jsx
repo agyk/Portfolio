992d0fcf3bd58911359d4754226c25da57c3b595
agyk <agines.kovacs@gmail.com>
2023-04-20 17:07:51 +0200

    links added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,18 +13,22 @@ function Projects(props){
                 <div className="work">
                     <img src={weather}/>
                     <p className="work_name">Weather App usig weather API</p>
+                    <a href="https://github.com/agyk/weathherAPI" target="_blank">code </a>
                 </div>
                 <div className="work">
                     <img src={imgGal} />
                     <p className="work_name">Image Gallery</p>
+                    <a href="https://github.com/agyk/PhotoGallery" target="_blank">code</a>
                 </div>
                 <div className="work">
                     <img src={warhol} />
                     <p className="work_name">Art gallery</p>
+                    <a href="https://github.com/agyk/ArtGallery" target="_blank">code</a>
                 </div>
                 <div className="work">
                     <img src={aniart} />
                     <p className="work_name">Artist landing page</p>
+                    <a href="https://github.com/agyk/ani-art" target="_blank">code</a>
                 </div>
             </div>
             
